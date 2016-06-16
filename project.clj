fdff739d84397052a7667926d9e9522978876406
Duncan McGreggor <oubiwann@gmail.com>
2016-06-16 18:14:17 -0500

    Updated project for dev fork deploy.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject spootnik/mesomatic "0.28.0-r0"
-  :description "Apache Mesos type conversions to and from Clojure"
-  :url "https://github.com/pyr/mesomatic"
+(defproject clojusc/mesomatic "0.28.2-SNAPSHPOT"
+  :description "Apache Mesos type conversions to and from Clojure (dev fork)"
+  :url "https://github.com/clojusc/mesomatic"
   :license {:name "MIT License"}
   :dependencies [[org.clojure/clojure    "1.8.0"]
-                 [org.apache.mesos/mesos "0.28.0"]])
+                 [org.apache.mesos/mesos "0.28.2"]])
